e3bf3cd4af4e843b65e9a1194556b0f437b90940
Harshit Rajan Srivastava <39988267+RajanHarshit@users.noreply.github.com>
2025-08-01 01:55:53 +0530

    + swagger files

--- loan-approval/main.go
+++ loan-approval/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"loan-approval/controllers"
 	"loan-approval/database"
+	_ "loan-approval/docs"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Loan Approval API
+// @version 1.0
+// @description API for managing and approving loan applications
+// @host localhost:8080
+// @BasePath /
 
 func main() {
 	r := gin.Default()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	database.ConnectDatabase()
 
 	r.POST("/apply", controllers.ApplyForLoan)
